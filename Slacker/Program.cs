3c33a34d391ceb84e360dba2df777e238e74b804
YumProjects <pilotben@comcast.net>
2018-05-24 07:32:02 -0700

    Adding things.

---
+++ Slacker/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Net;
+using System.IO;
+using System.Net.Http;
+
+namespace Slacker
+{
+    class Program
+    {
+        static string Token = "xoxp-166756780596-250867385107-368693279941-b9c4e0600d550b80f91a5eded5f1c4cc";
+        static Slack slack = new Slack(Token);
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Retrieving files list...");
+            JArray Files = GetFilesList();
+
+            foreach(JObject FileObj in Files)
+            {
+                string ID = FileObj.Value<string>("id");
+                string Name = FileObj.Value<string>("name");
+                string Url = FileObj.Value<string>("url_private");
+
+                Console.WriteLine("Downloading \"" + Name + "\"...");
+
+                string FilePath = "C:\\Users\\Benny\\Desktop\\Slack\\" + MakeValidFileName(Name);
+
+                byte[] Data = slack.DownloadSlackUrl(Url);
+
+                File.WriteAllBytes(FilePath, Data);
+            }
+
+            Console.ReadLine();
+        }
+
+        static JArray GetFilesList()
+        {
+            string Responce = slack.SendPostApiRequest("files.list");
+
+            JObject ListObj = JObject.Parse(Responce);
+
+            return ListObj.Value<JArray>("files");
+        }
+
+        static string MakeValidFileName(string FileName)
+        {
+            string ValidFileName = FileName;
+
+            foreach(char c in Path.GetInvalidFileNameChars())
+            {
+                ValidFileName = ValidFileName.Replace(c, '_');
+            }
+
+            return ValidFileName;
+        }
+    }
+}
---
+++ Slacker/Slack.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net;
+using System.Net.Http;
+
+namespace Slacker
+{
+    class Slack
+    {
+        public string Token;
+
+        public Slack(string Token)
+        {
+            this.Token = Token;
+        }
+
+        public string SendGetApiRequest(string ApiCall, params string[] Args)
+        {
+            string Request;
+            if (Args.Length == 0)
+            {
+                Request = "https://slack.com/api/" + ApiCall + "?token=" + Token;
+            }
+            else
+            {
+                Request = "https://slack.com/api/" + ApiCall + "?token=" + Token + "&" + string.Join("&", Args);
+            }
+
+            HttpClient Client = new HttpClient();
+            HttpContent content = new StringContent(Request);
+            content.Headers.Add("Authorization", "Bearer " + Token);
+            HttpResponseMessage Responce = Client.GetAsync(Request).Result;
+
+            return Responce.Content.ReadAsStringAsync().Result;
+        }
+
+        public string SendPostApiRequest(string ApiCall, params string[] Args)
+        {
+            string UrlContent = "token=" + Token + "&" + string.Join("&", Args);
+
+            HttpContent Content = new StringContent(UrlContent, Encoding.UTF8, "application/x-www-form-urlencoded");
+            HttpClient Client = new HttpClient();
+ 
+            HttpResponseMessage Responce = Client.PostAsync("https://slack.com/api/" + ApiCall, Content).Result;
+
+            return Responce.Content.ReadAsStringAsync().Result;
+        }
+
+        public byte[] DownloadSlackUrl(string Url)
+        {
+            HttpClient DownloadClient = new HttpClient();
+
+            HttpRequestMessage Request = new HttpRequestMessage(HttpMethod.Get, Url);
+
+            Request.Headers.Add("Authorization", "Bearer " + Token);
+
+            HttpResponseMessage Responce = DownloadClient.SendAsync(Request).Result;
+
+            return Responce.Content.ReadAsByteArrayAsync().Result;
+        }
+    }
+}
